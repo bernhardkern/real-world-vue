9eab7582ec8a37406ff61809af4f36f999abbbe1
Bernhard Kern <bernhard.kern@iits-consulting.de>
2021-12-23 16:46:23 +0100

    lazy loading

--- src/router/index.js
+++ src/router/index.js
@@ -5,7 +5,6 @@ import EventDetails from '../views/event/Details.vue'
 import EventRegister from '../views/event/Register.vue'
 import EventEdit from '../views/event/Edit.vue'
 import NetworkError from '../views/NetworkError.vue'
-import About from '../views/About.vue'
 import NotFound from '../views/NotFound.vue'
 import NProgress from 'nprogress'
 import GStore from '@/store'
@@ -48,7 +47,7 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    component: About,
+    component: () => import(/*webpackChunkName: "about"*/ '../views/About.vue'),
   },
   {
     name: 'NotFound',
